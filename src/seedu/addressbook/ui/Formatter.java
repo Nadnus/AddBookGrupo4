01c41514090202b28593a68a0bcc4e94ab5fbde7
Andre <andre.segovia@utec.edu.pe>
2019-10-18 12:52:19 -0500

    ADA SE PASA SOLO

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,5 +1,6 @@
 package seedu.addressbook.ui;
 
 public class Formatter {
-
+    public static final String LINE_PREFIX = "|| ";
+    public static final String DIVIDER = "===================================================";
 }
